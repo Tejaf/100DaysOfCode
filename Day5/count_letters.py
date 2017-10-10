6c0c6e732be07b454b8ad7d8c3ac31ab58a6895d
Afonja Tejumade <tmafonja1@gmail.com>
2017-10-10 09:46:16 +0100

    Day5 #100DaysOfCode

--- Day5/count_letters.py
+++ Day5/count_letters.py
@@ -1,5 +1,5 @@
 '''
-    Count Letters in a Speech* - Attached is a text file containing the farewell speech of Barack Obama. 
+    Count Letters in a Speech - Attached is a text file containing the farewell speech of Barack Obama. 
     The function should read content from the file, count and record all letters of the alphabet present. 
     Output should be like => a: 20, b: 10, ...z: 5. 
     It should also be case insensitive i.e 'a' == 'A'.
@@ -7,11 +7,12 @@
 
 import string
 def count_letters(filename):
-    alphabeth = string.ascii_lowercase
+    alphabeths = string.ascii_lowercase
     with open(filename) as f:
-        speech =  f.read().lower()
-        letter_count = {letter:speech.count(letter) for letter in alphabeth}
+        file_content =  f.read().lower()
+        letter_count = {letter:file_content.count(letter) for letter in alphabeths}
         print letter_count
 
 
-count_letters("obama_speech.txt")
+if __name__ == '__main__':
+    count_letters("obama_speech.txt")
