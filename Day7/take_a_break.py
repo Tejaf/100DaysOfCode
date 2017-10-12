28bacf62ea976a1879265cf52e73b78f2a7f38d2
Afonja Tejumade <tmafonja1@gmail.com>
2017-10-12 12:08:22 +0100

    Day7 #100DaysOfCode

--- Day7/take_a_break.py
+++ Day7/take_a_break.py
@@ -23,26 +23,27 @@ def take_a_break(break_interval, url):
 
     time.sleep(break_interval)
 
-    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-    webbrowser.get(chrome_path).open(url,new=new)
+    webbrowser.open(url, new=new)
 
 def main():
-    if sys.version_info[0] == 2:
-        input = raw_input
 
     print('''
             At what interval 'in min' would you like to take a break? e.g 1, 25
             press enter to use default 25min
             
             ''')
-    break_interval = int(input(">> ")) * 60
+    if sys.version_info[0] == 2:
+        inp = raw_input
+    else:
+        inp = input
+    break_interval = int(inp(">> ")) * 60
 
     print ('''
         What music would you like to play? provide a url e.g http://www.youtube.com/
         press enter to use default, "you're beautiful by zedd"
     
     ''')
-    url = str(input(">> "))
+    url = str(inp(">> "))
 
     while True:
         take_a_break(break_interval, url)
