4922d8cd48bb1a5945bb8b8fdb26305b1059d5c8
Afonja Tejumade <tmafonja1@gmail.com>
2017-10-17 15:09:35 +0100

    Day13 #100DaysOfCode
    Make sure code works for regular sorting of arrays

--- Day13/natural_sort.py
+++ Day13/natural_sort.py
@@ -7,12 +7,14 @@ import re
 '''
 
 def natural_sort(key):
-    convert_to_num = lambda text: int(text) if text.isdigit() else text.lower()
+    convert_to_num = lambda text: int(text) if text.isdigit() else text
     alphanum_key = [convert_to_num(c) for c in re.split(r'(\d+)', key)]
     return alphanum_key
 
 arr = ['a1','a6','a3','b1', 'c3', 'b2', 'a9','a11','a13']
-arr.sort(key= lambda a: natural_sort(a))
+# ar = ["a","b", "c", "A", "B", "C"]
+# aa = [1,2,7,2,3,8,9,4,5]
+arr.sort(key= lambda a: natural_sort(str(a)))
 
-print (arr) # ['a1', 'a3', 'a6', 'a9', 'a11', 'a13', 'b1', 'b2', 'c3']
+print (aa) # ['a1', 'a3', 'a6', 'a9', 'a11', 'a13', 'b1', 'b2', 'c3']
 
